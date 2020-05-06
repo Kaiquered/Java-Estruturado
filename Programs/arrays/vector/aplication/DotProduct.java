5fc8d71b87bcf004834258c3eab9de40f5d74a5a
Kaiquered <kaiquered@hotmail.com>
2020-05-06 15:44:26 -0300

    adicionando exemplo Dot Product

---
+++ Programs/arrays/vector/aplication/DotProduct.java
@@ -0,0 +1,42 @@
+package vector.aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DotProduct {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the number of dimensions: ");
+		int n = input.nextInt();
+
+		double sum = 0;
+
+		double[] vectorA = new double[n];
+		double[] vectorB = new double[n];
+
+		System.out.print("\nEnter elements Vector A: ");
+
+		for (int i = 0; i < n; i++) {
+			vectorA[i] = input.nextDouble();
+		}
+
+		System.out.print("Enter elements Vector B: ");
+
+		for (int i = 0; i < n; i++) {
+			vectorB[i] = input.nextDouble();
+		}
+
+		for (int i = 0; i < n; i++) {
+			sum += vectorA[i] * vectorB[i];
+		}
+
+		System.out.printf("\nThe dot product is: %.2f", sum);
+		
+		input.close();
+	}
+}
